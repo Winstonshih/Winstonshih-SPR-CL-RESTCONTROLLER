d8c0b083abc2f2a4466aeacfe23da880f811acdf
Winstonshih <Winstonshih@github.com>
2026-09-12 19:03:30 +0000

    Test run: 2/2 passed, 0 failed, 0 skipped - 2026-09-12T19:03:30.083Z

--- src/main/java/Lab/Controller/SampleController.java
+++ src/main/java/Lab/Controller/SampleController.java
@@ -47,9 +47,9 @@ public class SampleController {
      * should respond with "1".
      */
     @GetMapping("/long/{id}")
-    public long getPathVariable(){
+    public long getPathVariable(@PathVariable long id){
         //you will need to change the method's parameters and return the extracted path variable.
-        return "Path Variable: "+id;
+        return id;
     }
     /**
      * TODO: extract the request body and respond with it.
